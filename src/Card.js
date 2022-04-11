5a8ca0947e37f1d22f0bfc366b6459c36bcd87fb
Yunjeong <8768moon@naver.com>
2022-04-11 10:34:28 +0900

    props

--- src/Card.js
+++ src/Card.js
@@ -3,8 +3,10 @@ import React from 'react';
 function Card(props) {
 	return (
 		<article>
-			<div className='pic'>
-				<img src={`${props.path}/img/${props.data}.jpg`} />
+			<div className='inner'>
+				<div className='pic'>
+					<img src={`${props.path}/img/${props.data}.jpg`} />
+				</div>
 			</div>
 		</article>
 	);
